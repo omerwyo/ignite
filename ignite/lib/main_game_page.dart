b0ef15c0fcc75e1aa68534f94107652ef23680e1
darrylpoh <darrylpoh.dp13@gmail.com>
2022-09-29 08:21:32 +0800

    Initiate ContriSide

---
+++ ignite/lib/main_game_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'helpers/direction.dart';
+import 'helpers/joypad.dart';
+
+class MainGamePage extends StatefulWidget {
+  const MainGamePage({Key? key}) : super(key: key);
+
+  @override
+  MainGameState createState() => MainGameState();
+}
+
+class MainGameState extends State<MainGamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+        body: Stack(
+          children: [
+            // TODO 1
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Joypad(onDirectionChanged: onJoypadDirectionChanged),
+              ),
+            )
+          ],
+        ));
+  }
+
+  void onJoypadDirectionChanged(Direction direction) {
+    // TODO 2
+  }
+}
