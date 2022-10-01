99e6de975d383668a8605060e44d6595f0dafac6
omerwyo <omerbaggia123@gmail.com>
2022-10-01 18:00:50 +0800

    leaderboard drawer

--- ignite/lib/main_game_page.dart
+++ ignite/lib/main_game_page.dart
@@ -54,29 +54,77 @@ class LogInPage extends StatefulWidget {
   LogInState createState() => LogInState();
 }
 
-
 class LogInState extends State<LogInPage> {
+  final String hamburger = 'assets/images/hamburger.svg';
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
-          extendBodyBehindAppBar: true,
-          appBar: AppBar(
-            iconTheme: IconThemeData(
-              color: Colors.black,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
               ),
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-        ), 
-        body: Container(
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Map'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Leaderboard'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Search'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        // ignore: sort_child_properties_last
+        child: Center(
+          child: Text('Leaderboard'),
+          heightFactor: 190.0,
+          widthFactor: MediaQuery.of(context).size.width - 100.0,
+        ),
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/background.png"),
             fit: BoxFit.cover,
           ),
         ),
-        child: null
       ),
     );
-        
   }
 }
